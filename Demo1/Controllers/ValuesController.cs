624a4cd538693ff2b73d51cbc1ef89157ae998b0
Administrator <Administrator@DESKTOP-DDFGGPH>
2024-12-29 23:23:46 -0800

    done with custom routing

--- Demo1/Controllers/ValuesController.cs
+++ Demo1/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Demo1.Controllers
@@ -17,5 +18,17 @@ namespace Demo1.Controllers
         {
             return id;
         }
+
+        [HttpGet]
+        [Route("user/{phoneNumber:regex(^09\\d{{9}})}/{mail:regex(^[[a-z A-Z 0-9 . _ % + -]]+@[[a-z A-Z 0-9 . -]]+\\.[[a-z A-Z]]{{2,}}$)}")]
+        public IActionResult Info(string phoneNumber,string mail)
+        {
+            //var regex = new Regex(@"^[a-z A-Z 0-9 . _ % + -]+@[a-z A-Z 0-9 . -]+\.[a-z A-Z]{2,}$");
+            //if (!regex.IsMatch(mail))
+            //{
+            //    return NotFound();
+            //}
+            return Content("phone : " + phoneNumber + " mail " + mail);
+;        }
     }
 }
