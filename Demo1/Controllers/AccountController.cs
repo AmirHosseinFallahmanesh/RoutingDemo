2c69c7c277aa06d67d7c2a53bda55c45bd4e4036
Administrator <Administrator@DESKTOP-DDFGGPH>
2024-12-29 22:11:27 -0800

    Add project files.

---
+++ Demo1/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("login page");
+        }
+
+        public IActionResult GetUserInfo(int id)
+        {
+            return Content("user info  id = " + id);
+        }
+    }
+}
---
+++ Demo1/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo1.Controllers
+{
+    public class BlogController : Controller
+    {
+        
+        public IActionResult Index(int year,string month,int day)
+        {
+            return Content($"news for {year}/{month}/{day}");
+        }
+    }
+}
